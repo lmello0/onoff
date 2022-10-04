d99e01dc91a0ace0f06508b8f7740c8266cf4236
lmello0 <melloluc02@gmail.com>
2022-10-04 16:54:55 -0300

    Adding methods to known_hosts class

--- onoff/src/main/java/com/onoff/Known_Hosts.java
+++ onoff/src/main/java/com/onoff/Known_Hosts.java
@@ -1,29 +1,62 @@
 package com.onoff;
 
-import org.json.*;
-
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class Known_Hosts {
     private File jsonFile = new File("known_hosts/known_hosts.json");
-    private Scanner jsonObject = null;
+    private JSONArray devices;
 
-    public Known_Hosts() throws FileNotFoundException {
-        jsonObject = new Scanner(jsonFile);
+    public Known_Hosts() {
+        try (FileReader reader = new FileReader(jsonFile)) {
+            devices = (JSONArray) new JSONParser().parse(reader);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (org.json.simple.parser.ParseException e) {
+            e.printStackTrace();
+        }
     }
 
-    public String getJsonString(){
-        return jsonObject.toString();
+    public String get_ip(String device_name) {
+        for(int i = 0; i < devices.size(); i++) {
+            JSONObject device = parseDeviceObject((JSONObject) devices.get(i));
+            
+            if (device.get("name").equals(device_name)) {
+                return (String) device.get("ip");
+            }
+        }
+
+        return "Not found ip with the given device name!";
+    }
+
+    public String get_mac(String device_name) {
+        for(int i = 0; i < devices.size(); i++) {
+            JSONObject device = parseDeviceObject((JSONObject) devices.get(i));
+            
+            if (device.get("name").equals(device_name)) {
+                return (String) device.get("mac");
+            }
+        }
+
+        return "Not found MAC with the given device name!";
+    }
+
+    private JSONObject parseDeviceObject(JSONObject device) {
+        return (JSONObject) device.get("device");
+
     }
 
     public static void main(String[] args) {
-        try {
-            Known_Hosts known_Hosts = new Known_Hosts();
-            System.out.println(known_Hosts.getJsonString());  
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+        Known_Hosts known_hosts = new Known_Hosts();
+
+        System.out.println(known_hosts.get_mac("lmello-ubuntuhomeserver"));
     }
 }
